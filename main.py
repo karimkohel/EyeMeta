0ba5f5f8c47662f6709b430b8dd77ea14d91f0c7
karimkohel <kareemkohel@gmail.com>
2023-03-23 12:57:38 +0200

    wrote main plan for app

---
+++ main.py
@@ -0,0 +1,3 @@
+# gather x,y coordinates for (with pyautogui) the entire run (after 30 seconds of starting or after finding a match for a game start flag)
+# write it all down with pandas
+# generate heatmap with seaborn sns.kdeplot
