e49d5de224e191bab689c5f1b40756f948e8279a
karimkohel <kareemkohel@gmail.com>
2023-03-23 15:07:40 +0200

    moved logic to separate class and enhanced logging with microsecond accuracy

---
+++ logger.py
@@ -0,0 +1,35 @@
+import pyautogui as pag
+import csv, time
+from datetime import datetime
+
+
+class CSVLogger():
+    def __init__(self, sps: int = 2) -> None:
+        """Start the logger class to log coordinates in csv file with time stamp
+
+        Parameters:
+        -----------
+        sps: optional, samples per second will default to 2 per second, the amount of coordinate samples to gather per second
+        
+        """
+        self.startDateTime = datetime.now().strftime("%d-%m-%Y_%H.%M")
+        self.fieldNames = ['x', 'y', 'time']
+        self.sps = sps
+
+    def startLogger(self) -> None:
+        self.csvFile = open(f'gazeData_{self.startDateTime}.csv', mode='w', newline='')
+        self.writer = csv.DictWriter(self.csvFile, fieldnames=self.fieldNames)
+        self.writer.writeheader()
+
+    def logCoordinates(self) -> None:
+        time.sleep(1/sps)
+        row = {
+            self.fieldNames[0]: pag.position().x,
+            self.fieldNames[1]: pag.position().y,
+            self.fieldNames[2]: datetime.now().strftime("%H:%M:%S.%f")
+        }
+        self.writer.writerow(row)
+
+    def __del__(self) -> None:
+        self.csvFile.close()
+        
--- main.py
+++ main.py
@@ -2,31 +2,16 @@
 # write it all down with pandas
 # generate heatmap with seaborn sns.kdeplot
 
+from logger import CSVLogger
+import time
 
-import pyautogui as pag
-import csv, time
-from datetime import datetime
-
-startDateTime = datetime.now().strftime("%d-%m-%Y_%H.%M")
-fieldNames = ['x', 'y', 'time']
-
+logger = CSVLogger()
 # buffer to give the user enough time to start the game
 time.sleep(5)
-
-csvFile = open(f'gazeData_{startDateTime}.csv', mode='w', newline='')
-writer = csv.DictWriter(csvFile, fieldnames=fieldNames)
-writer.writeheader()
+logger.startLogger()
 
 while True:
     try:
-        time.sleep(0.5)
-        row = {
-            fieldNames[0]: pag.position().x,
-            fieldNames[1]: pag.position().y,
-            fieldNames[2]: datetime.now().strftime("%H:%M:%S"),
-        }
-        writer.writerow(row)
+        logger.logCoordinates()
     except KeyboardInterrupt:
         break
-
-csvFile.close()
