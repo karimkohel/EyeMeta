ecd8039cae8576c6454bfb84d6ac19886dd3fd82
karimkohel <kareemkohel@gmail.com>
2023-03-23 16:55:09 +0200

    started basic heatmap generating

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 # gather x,y coordinates for (with pyautogui) the entire run (after 30 seconds of starting or after finding a match for a game start flag)
-# write it all down with pandas
+# write it all down as csv
+# read with pandas
 # generate heatmap with seaborn sns.kdeplot
+# figure out what more statistics do we need
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('gazeData_23-03-2023_16.40.csv')
+
+kde = sns.kdeplot(
+    x=df['x'],
+    y=df['y'],
+    fill=True,
+    thresh=0.05,
+)
+kde.invert_yaxis()
+plt.show()
